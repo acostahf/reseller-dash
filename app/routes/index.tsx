f6f57343036f90053588e443fc0c36af31a520b0
Fabian <acostahf4@gmail.com>
2022-09-19 20:40:50 -0500

    first commit with tailwind install

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+			<h1 className="text-red-600 font-bold text-4xl">Welcome to Remix</h1>
+		</div>
+	);
+}
